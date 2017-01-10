9a686d7622fa77152289f6df7da5686e2f835385
dude719 <blood_099@hotmail.com>
2017-01-10 11:18:57 -0400

    update

--- dumptable.py
+++ dumptable.py
@@ -1,24 +1,20 @@
 import sys
-
-from idautils import *
-from idc import *
+import idaapi
 
 def printf(format, *args):
     sys.stdout.write(format % args)
 
 def main():
-    imagebase = idaapi.get_imagebase()
-    address = idaapi.askaddr(imagebase + 0x19000, "Enter address of table: ")
+    address = idaapi.askaddr(idaapi.get_imagebase() + 0x19000, "Enter address of table: ")
     if (address != None):
         idx = 0
         while (address != BADADDR):
-            name = GetTrueName(Qword(address))
+            name = idaapi.get_true_name(BADADDR, Qword(address))
             if not name:
-                break
-            printf("[%04d] %-10s\n", idx, name)
-            address += 8
+                break 
+            printf("[%04d] %s = %X\n", idx, name, Qword(address))     
+            address += 8 if idaapi.get_inf_structure().is_64bit() else 4   
             idx += 1
         
-
 if __name__ == "__main__":
     main()
